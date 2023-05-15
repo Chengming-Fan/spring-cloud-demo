0be8ae6f9b8f4b81e0cd4e911c1c4cb28befbc53
chengming.fan <chengming.fan@thoughtworks.com>
2023-05-15 16:51:30 +0800

    fix gradle build problem

---
+++ micro-service-cloud-api/src/main/java/com/fan/api/Application.java
@@ -0,0 +1,14 @@
+package com.fan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Chengming Fan on 2023/5/15 16:50
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
