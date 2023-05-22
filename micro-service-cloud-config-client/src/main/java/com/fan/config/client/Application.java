d15b118ebb80bb3393755dd1173939f4bda495fb
chengming.fan <chengming.fan@thoughtworks.com>
2023-05-22 10:40:21 +0800

    add config client and update manually

---
+++ micro-service-cloud-config-client/src/main/java/com/fan/config/client/Application.java
@@ -0,0 +1,16 @@
+package com.fan.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by Chengming Fan on 2023/5/22 09:48
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
